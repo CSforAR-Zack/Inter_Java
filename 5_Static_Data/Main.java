47b73d53ebdadc35c7503305a6c7baa930b802db
Zackary <zspink@nea.k12.ar.us>
2023-05-23 12:14:23 -0500

    Removed JavaFX

--- 5_Static_Data/Main.java
+++ 5_Static_Data/Main.java
@@ -1,113 +1,94 @@
-// Program: Weather Data
-
-// We use the JavaFX library to create a line chart
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
-import javafx.stage.Stage;
-
-// General Java libraries
+// Importing libraries
+import javax.swing.*;
 import java.io.*;
 import java.util.*;
 import java.text.*;
-import java.util.Scanner;
+// Third party libraries
+import org.jfree.chart.*; 
+import org.jfree.data.time.*;
+import com.opencsv.*;
 
-// This is the class declaration
-// it inherits from Application (year 3) but we need it to make the line chart
-public class Main extends Application {
-    // start comes from the Application class through inheritance and is called when the program starts
-    // We add the @Override annotation to tell the compiler that we are overriding the method
-    // from the parent class. Override is a year 3 topic.
-    @Override
-    public void start(Stage stage) {        
-        stage.setTitle("Weather Data");
-        
-        // Create the axes
-        final NumberAxis xAxis = new NumberAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        // For simiplicity, we use days from 1-1-21 as the x axis
-        // There are ways to use Date objects, but they are more complicated
-        xAxis.setLabel("Day from 1-1-21");
+// Main extends JFrame to create a window
+// Main creates a chart and adds it to the window
+public class Main extends JFrame {
+    // Main constructor
+    public Main(){
+        // Create dataset as a TimeSeriesCollection
+        // TimeSeriesCollection comes from JFreeChart
+        TimeSeriesCollection dataset = createDataset();
+        // Create chart
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+                "Weather Data", // Title
+                "Date", // X-Axis Label
+                "Temperature (F)", // Y-Axis Label
+                dataset // Data to plot
+        );
+        // Add chart to panel to display
+        ChartPanel panel = new ChartPanel(chart);
+        setContentPane(panel);
+    }
 
-        // Create the line chart
-        final LineChart<Number,Number> lineChart = new LineChart<Number,Number>(xAxis,yAxis);                
-        lineChart.setTitle("Little Rock Temperatures");
-        
-        // Create the different series that will be displayed on the line chart
-        XYChart.Series seriesMinTemp = new XYChart.Series();
-        seriesMinTemp.setName("Min. Temp");
+    // Create dataset from CSV file
+    private TimeSeriesCollection createDataset(){
+        // Create two series to store min and max temperatures
+        TimeSeries series1 = new TimeSeries("Min. Temperatures");
+        TimeSeries series2 = new TimeSeries("Max Temperatures");
 
-        XYChart.Series seriesMaxTemp = new XYChart.Series();
-        seriesMaxTemp.setName("Max Temp");
-        
-        // Create an ArrayList to store the data and load the data from the file
-        List<String[]> data = new ArrayList<>();        
+        // Load data from CSV file
+        List<String[]> data = new ArrayList<>();
         data = LoadDataFromFile();
-        // Loop through the data and add it to the correct series
-        // Since some columns had commas in them, we need to take that into account
-        // We could consider using a CSV library to make this easier or
-        // We could use a regular expression to split the line on commas
-        for (int i = 1; i < data.size(); i++){
-            int minTemp = Integer.parseInt(data.get(i)[8]);
-            int maxTemp = Integer.parseInt(data.get(i)[7]);
-            seriesMinTemp.getData().add(new XYChart.Data(i, minTemp));
-            seriesMaxTemp.getData().add(new XYChart.Data(i, maxTemp));
+
+        // Create date formatter to parse dates from CSV file
+        // so they can be added to the series
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+        // Loop through data and add to appropriate series
+        for (int i = 1; i < data.size(); i++) {
+            String stringDate = data.get(i)[2];
+            Date date = new Date();
+
+            // Parse needs to be in try/catch block
+            try {
+                date = formatter.parse(stringDate);
+            } catch(ParseException e){
+                System.out.println(e);
+            }
+            int minTemp = Integer.parseInt(data.get(i)[7]);
+            int maxTemp = Integer.parseInt(data.get(i)[6]);
+            // Day is a class from JFreeChart
+            series1.add(new Day(date), minTemp);
+            series2.add(new Day(date), maxTemp);
+        }
+
+        // Create the collection and add the series
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        dataset.addSeries(series1);
+        dataset.addSeries(series2);
+
+        return dataset;
+    }
+    
+    // Load data from CSV file
+    public static List<String[]> LoadDataFromFile() {
+        List<String[]> data = new ArrayList<>();
+
+        // Working with files needs to be in try/catch block
+        try {
+            CSVReader reader = new CSVReader(new FileReader("weather_lr.csv"));
+            data = reader.readAll();
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
         }
         
-        // Create the scene and add the line chart to it
-        Scene scene  = new Scene(lineChart,800,600);
-        // We use a css file to style the line chart for simplicity
-        // This could be done in code as well, but documentation is sparse
-        scene.getStylesheets().add("style.css");
-        // Add the series to the line chart
-        lineChart.getData().addAll(seriesMinTemp, seriesMaxTemp);
-
-        // Change the look of the lines
-        lineChart.setCreateSymbols(false);
-
-        // This is an alternative way to change the look of the lines
-        // seriesMinTemp.getNode().setStyle("-fx-stroke: blue;");
-        // seriesMaxTemp.getNode().setStyle("-fx-stroke: red;");
-        
-        // Set the scene and show the stage
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    // This method loads the data from the file
-    // It returns a list of String arrays
-    // Each String array is a line from the file
-    // Each element in the String array is a column from the file
-    // However, the first line is the column headers
-    public static List<String[]> LoadDataFromFile(){
-        List<String[]> data = new ArrayList<>();
-        
-        // This is a try catch block and is required when using File IO
-        try{
-            // We will use a Scanner to read the file, however, there are other ways to do this
-            Scanner sc = new Scanner(new File("weather_lr.csv"));
-            while (sc.hasNextLine()){
-                // We read the line and split it into an array of Strings
-                // We remove the quotes from the line for easier processing later
-                // We split the line on the comma. Keep in mind that some of the columns have commas in them
-                // We will deal with this later
-                String[] line = new String[9];
-                String rawLine = sc.nextLine().replace("\"", "");
-                line = rawLine.split(",");
-                data.add(line);
-            }
-            // Close the scanner so we don't have a resource leak
-            sc.close();              
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-        }        
         return data;
     }
- 
+
     public static void main(String[] args){
-        // This is how we launch the JavaFX application
-        launch(args);
-    }        
+        // Create chart
+        Main chartWindow = new Main();
+        chartWindow.pack();
+        chartWindow.setSize(600, 400);
+        chartWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        chartWindow.setVisible(true);
+    }
 }
--- 7_Dice_OOP/Main.java
+++ 7_Dice_OOP/Main.java
@@ -1,69 +1,71 @@
-// Purpose: Simulate rolling two dice numerouse times and display the sum of the results in a bar chart.
-
-// These are the imports
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.XYChart;
-import javafx.stage.Stage;
-
+// Libraries to import
+import javax.swing.*;
 import java.io.*;
 import java.util.*;
 import java.text.*;
-import java.util.Date;
-import java.util.Scanner;
+import java.awt.*;
+// Third party libraries
+import org.jfree.chart.*; 
+import org.jfree.chart.plot.*;
+import org.jfree.data.category.*;
+import org.jfree.chart.renderer.category.BarRenderer;
  
- 
-public class Main extends Application {
-    // These are the variables that we can change to change the simulation
-    // We put them outside of the start method so we can easily change them
+// extends JFrame to create a window
+public class Main extends JFrame {
+    // Number of rolls to simulate and dice to use
     int numberOfRolls = 100000;
     Die die1 = new Die(20);
     Die die2 = new Die(20);
     
-    // We discussed the @Override annotation in earlier
-    @Override
-    public void start(Stage stage) {
-        // Set the title and axes labels
-        stage.setTitle("Dice Roll Simulation");
-        final CategoryAxis xAxis = new CategoryAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("Sum (Die1 + Die2) Frequencies");
+    public Main() {
+        // Create the chart      
+        JFreeChart barChart = ChartFactory.createBarChart(
+            "Dice Roll Simulation", // Chart title        
+            "Sum (Die1 + Die2)", // X-Axis Label         
+            "Frequencies", // Y-Axis Label         
+            createDataset(), // Dataset        
+            PlotOrientation.VERTICAL, // Plot Orientation       
+            false, false, false // Show Legend, Tooltips, and URLs
+        );
+        
+        // Set the chart properties
+        BarRenderer renderer = (BarRenderer) barChart.getCategoryPlot().getRenderer();
+        // Set the bar width, -20 makes the bars look nice and close together
+        renderer.setItemMargin(-20);
+        // Set the bar colors to blue
+        for (int i = 0; i <= 38; i++){
+            renderer.setSeriesPaint(i, Color.blue);            
+        }
 
-        // Create the bar chart
-        final BarChart<String,Number> barChart = new BarChart<String,Number>(xAxis, yAxis);
-        // Create the series that will be displayed on the bar chart
-        // We will only use one series, but we could use more
-        XYChart.Series rolls = new XYChart.Series();
-        rolls.setName("Rolls");
+        // Set the font size of the x-axis labels to fit
+        CategoryPlot plot = barChart.getCategoryPlot();
+        Font font = new Font("Dialog", Font.PLAIN, 10); 
+        plot.getDomainAxis().setTickLabelFont(font);
+        
+        // Add the chart to a panel
+        ChartPanel chartPanel = new ChartPanel(barChart);       
+        setContentPane(chartPanel); 
+    }
+    
+    // Create the dataset
+    private CategoryDataset createDataset( ) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
-        // Simulate the rolls and add them to the series
-        HashMap<Integer, Integer> rollingSimResults = new HashMap<Integer, Integer>();
-        rollingSimResults = SimulateRolling(die1, die2, numberOfRolls);
+        HashMap<Integer, Integer> rolls = SimulateRolling(die1, die2, numberOfRolls);
 
-        for (Integer key : rollingSimResults.keySet()){
+        for (Integer key : rolls.keySet()){
             String x = key.toString();
-            Integer y = rollingSimResults.get(key);
-            rolls.getData().add(new XYChart.Data(x, y));       
+            Integer y = rolls.get(key);
+            // Add the data to the dataset y, x, series name
+            // We hack the series name to be the same as the x-axis label
+            dataset.addValue(y, x, x);  
         }
         
-        Scene scene  = new Scene(barChart,800,600);
-        barChart.getData().addAll(rolls);
-
-
-        barChart.setCategoryGap(1);
-        stage.setScene(scene);
-        stage.show();
+        return dataset; 
     }
 
-    // This method simulates rolling two dice a number of times
-    // It returns a HashMap with the sum of the rolls as the key and the number of times it occurred as the value
-    // We use a HashMap because it makes it easier to store the sum and the number of times it occurred
-    // We could use a 2D array, but it would be more complicated
-    // We could also use two ArrayLists, but that would add more complexity
-    public static HashMap<Integer, Integer> SimulateRolling(Die die1, Die die2, int numberOfRolls){
+    // Simulate rolling the dice
+    private HashMap<Integer, Integer> SimulateRolling(Die die1, Die die2, int numberOfRolls){
         ArrayList<Integer> rolls = new ArrayList<Integer>();
         
         for (int i = 0; i < numberOfRolls; i++){
@@ -72,16 +74,21 @@ public class Main extends Application {
         }
 
         HashMap<Integer, Integer> frequencies = new HashMap();
-        int rollSum = die1.GetNumberOfSides() + die2.GetNumberOfSides();
-            
+
+        int rollSum = die1.GetNumberOfSides() + die2.GetNumberOfSides();            
         for (int i = 2; i <= rollSum; i++){
             Integer occurrences = Collections.frequency(rolls, i);
             frequencies.put(i, occurrences);
         }
+        
         return frequencies;
     }
- 
+    
     public static void main(String[] args){
-        launch(args);
+        Main example = new Main();
+        example.pack();
+        example.setSize(800, 400);
+        example.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        example.setVisible(true);
     }        
 }
